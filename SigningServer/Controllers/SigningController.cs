daa3f31548b03a102fc5b86df35805f5dda149a5
Simon Christiansen <simonchris1729@gmail.com>
2020-02-19 18:04:46 +0100

    Initial commit

---
+++ SigningServer/Controllers/SigningController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using JuraDemo.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SigningServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SigningController : ControllerBase
+    {
+        private readonly string _rootPath;
+
+        public SigningController(
+            IWebHostEnvironment environment
+        )
+        {
+            _rootPath = environment.WebRootPath;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello");
+        }
+
+        [HttpPost]
+        public IActionResult SignPDF([FromBody] byte[] pdf)
+        {
+            using(var stream = new MemoryStream(pdf)) {
+                var signedPDF = PDFSigningService.SignPDFStream(stream, _rootPath);
+                return Ok(signedPDF);
+            }
+        }
+    }
+}
